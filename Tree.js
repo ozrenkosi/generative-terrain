d477318788f323c523bbea5e93c19fd864c3dce3
Ozren Kosi <ozren.kosi@gmail.com>
2025-08-21 18:24:12 +0200

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -1,16 +1,16 @@
 class Tree {
-  draw(boxHeight) {
+  draw(boxPixelHeight) {
     noStroke();
 
     push();
 
-    translate(0, 0, (boxHeight / 2) + (boxSize / 2));
+    translate(0, 0, (boxPixelHeight / 2) + (boxWidth / 2));
     fill(colors.treeTrunk);
-    box(boxSize / 6, boxSize / 6, boxSize);
+    box(boxWidth / 6, boxWidth / 6, boxWidth);
 
-    translate(0, 0, (boxSize / 1.5) / 2);
+    translate(0, 0, (boxWidth / 1.5) / 2);
     fill(colors.treeLeaves);
-    box(boxSize / 2, boxSize / 2, boxSize / 1.5);
+    box(boxWidth / 2, boxWidth / 2, boxWidth / 1.5);
 
     pop();
   }
